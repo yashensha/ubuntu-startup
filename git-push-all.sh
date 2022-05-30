914795c0fc8b074dd7d815ee56a703a12c3b13d3
yashensha <m.yashensha@gmail.com>
2022-05-30 20:51:33 +0530

    30-05-2022-33

---
+++ git-push-all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+now=$(date +"%d-%m-%Y-%S")
+echo "checking repo status...."
+git status;
+echo "adding all changed files...."
+git add .;
+echo "commiting changes...."
+git commit -m "$now";
+echo "pushing to git repo...."
+git push -u origin master;
