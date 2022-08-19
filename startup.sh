1800409bed7c3aac7f7de53e95397e3fde198f02
yashensha <m.yashensha@gmail.com>
2022-08-19 18:07:48 +0530

    Added color to commands

--- startup.sh
+++ startup.sh
@@ -1,56 +1,63 @@
 #!/bin/bash
 
-echo "Installing vlc...."
-sudo snap install vlc;
-echo "vlc installed."
-echo "Installing git...."
-sudo add-apt-repository ppa:git-core/ppa;
-sudo apt update;
-sudo apt install git;
-echo "git installed."
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 
-
-echo "Installing vs-code....."
-sudo apt update;
+sudo apt update -y;
 sudo apt upgrade -y;
+
+echo -e "${GREEN}Installing vlc....${ENDCOLOR}"
+sudo snap install vlc -y;
+echo -e "${GREEN}vlc installed.${ENDCOLOR}"
+
+echo -e "${GREEN}Installing git....${ENDCOLOR}"
+sudo add-apt-repository ppa:git-core/ppa;
+sudo apt install git -y;
+echo -e "${GREEN}git installed.${ENDCOLOR}"
+
+
+echo -e "${GREEN}Installing VSCode....${ENDCOLOR}"
 sudo apt install software-properties-common apt-transport-https wget;
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -;
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main";
-sudo apt install code;
+sudo apt install code -y;
+echo -e "${GREEN}VSCode installed.${ENDCOLOR}"
 
-sudo apt update && sudo apt upgrade -y;
-echo "vs-code installed."
 
-echo "Installing brave-browser...."
-
-sudo apt install apt-transport-https curl;
+echo -e "${GREEN}Installing Brave Browser....${ENDCOLOR}"
+sudo apt install apt-transport-https curl -y;
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg;
 
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list;
 
-sudo apt update;
 
-sudo apt install brave-browser;
+sudo apt install brave-browser -y;
 
-echo "brave-browser installed."
+echo -e "${GREEN}Brave Browser installed.${ENDCOLOR}"
 
-echo "Uninstalling firefox...."
+echo -e "${RED}Uninstalling firefox....${ENDCOLOR}"
 
-sudo snap remove firefox;
-sudo apt-get purge firefox;
+sudo snap remove firefox -y;
+sudo apt-get purge firefox -y;
 sudo rm -rf .mozilla/firefox/;
 sudo rm -rf .macromedia/;
 sudo rm -rf /etc/firefox/;
 sudo rm -rf /usr/lib/firefox/;
 sudo rm -rf /usr/lib/firefox-addons/;
 
-echo "firefox removed."
+echo -e "${RED}firefox removed.${ENDCOLOR}"
 
 
-echo "Installing nodejs."
-sudo apt install nodejs;
-echo "Installing npm."
-sudo apt install npm;
+echo -e "${GREEN}Installing nodejs.${ENDCOLOR}"
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs;
 
-echo "all execution completed"
+
+echo -e "${GREEN}Installing npm.${ENDCOLOR}"
+sudo npm -g i npm;
+
+echo -e "${GREEN}all execution completed${ENDCOLOR}"
+
+sudo apt-get autoclean -y;
 read abc
